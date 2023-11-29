92be0a691fafc69b88e4dd3c32db55df54fde9f4
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2023-11-29 16:09:11 +0100

    feat(app): add support for fetching vocabulary list from Airtable and store it in the store feat(app): import and use MarkdownModule in app module feat(app): add global styles to styles.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AirtableService } from './core/services/airtable.service';
+import { Store } from './store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'english-vocabulary';
+
+  constructor(private airTableService: AirtableService, private store: Store) {}
+
+  ngOnInit(): void {
+    this.airTableService.getVocabularyTable().subscribe((records) => {
+      this.store.set('vocabularyList', records);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MarkdownModule } from 'ngx-markdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Store } from './store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MarkdownModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [Store],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
