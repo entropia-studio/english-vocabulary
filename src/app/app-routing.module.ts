45fc723938d78e0f4ba3e771ebf12b3513fd2eac
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2023-11-29 16:09:41 +0100

    feat(app-routing.module.ts): add routes for 'terms' and 'descriptions' pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'terms',
+    loadChildren: () =>
+      import('./pages/terms/terms.module').then((m) => m.TermsModule),
+  },
+  {
+    path: 'descriptions',
+    loadChildren: () =>
+      import('./pages/descriptions/descriptions.module').then(
+        (m) => m.DescriptionsModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'terms',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
